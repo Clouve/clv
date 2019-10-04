17d819a3a25a3f54535506eff99022d4eb354580
AJ <aj@clouve.com>
2019-10-04 17:18:45 +0000

    allowing global npm install

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-#uninstall existing clv if any
+# --- uninstall existing clv if any --- #
 if [[ -e ~/.bash_profile ]] && grep -q "#ADDED BY CLV INSTALLER#" ~/.bash_profile; then
     ./uninstall.sh
 fi
 
-#install clv
+# --- install clv --- #
 mkdir ~/.clv
-cp -r "$SCRIPTPATH/commands/" ~/.clv
+mkdir ~/.clv/bin
+cp -r "$SCRIPTPATH/bin/" ~/.clv/bin
 touch ~/.bash_profile
-echo "export PATH=\"~/.clv:$PATH\" #ADDED BY CLV INSTALLER#" > ~/.bash_profile
+echo "export PATH=\"~/.clv/bin:\$PATH\" #ADDED BY CLV INSTALLER#" > ~/.bash_profile
 
 echo "clv has been installed successfully!"
